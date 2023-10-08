76bcb8c91f6e202e9c4f0441f285358de02931cd
Louis Blonde <loul0uboypy@gmail.com>
2023-10-08 15:29:51 +0200

    update footer

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -2,20 +2,31 @@ import React from "react";
 import gitLogo from "../../img/gitLogo.png";
 
 const Footer = () => {
+  const linkGit = "https://www.instagram.com/direct/inbox/";
   return (
     <div className="footing">
       <div className="individualBoxFooter">
-        <p className="textBoxFooter text">Developed by</p>
+        <p className="textBoxFooter text">
+          Developed by 
+          <a
+            className="textBoxFooter atext"
+            href={linkGit}
+            target="_blank"
+            without
+            rel="noreferrer"
+          >
+            Louis Blonde
+          </a>
+        </p>
         <a
-          className="individualBoxFooter textBoxFooter atext"
-          href="https://www.instagram.com/direct/inbox/"
+          className="atext"
+          href={linkGit}
           target="_blank"
           without
           rel="noreferrer"
         >
-          Louis Blonde
+          <img className="imgLogoGit" src={gitLogo} alt="gitLogoImage" />
         </a>
-        <img className="imgLogoGit" src={gitLogo} alt="gitLogoImage" />
       </div>
     </div>
   );
