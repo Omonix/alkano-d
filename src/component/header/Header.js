aa023bd3cc91fb737295dea1c7d5187bb0f7682b
Louis Blonde <loul0uboypy@gmail.com>
2023-12-16 13:07:14 +0100

    update Header component

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../logo/Logo.js";
-import Searcher from "../searcher/Searcher.js";
 import ReactSwitch from "react-switch";
 
 const Header = ({ moder, changer }) => {
+  const [searcher, setSearcher] = useState("");
+  const searcherHandler = (event) => {
+    setSearcher(event.target.value);
+  };
   return (
     <div className="header">
-      <Logo element={moder} />
-      <Searcher />
+      <div className="Logo">
+        <img
+          className="logo"
+          src={
+            moder === "light"
+              ? "../../logo/lightMode.png"
+              : "../../logo/darkMode.png"
+          }
+          alt="test"
+        />
+      </div>
+      <div className="searcher">
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Search a torrent"
+          value={searcher}
+          onChange={searcherHandler}
+        />
+        <button>Search</button>
+      </div>
       <div className="switch">
         <label className="labelMode text">Dark mode</label>
         <ReactSwitch onChange={() => changer()} checked={moder === "dark"} />
